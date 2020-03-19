656c6e1a01a4664653203230b1084f370c5093e0
cyta <piotrstachnio@gmail.com>
2020-03-19 14:15:41 +0100

    implementing some changes

--- zadanieFork.c
+++ zadanieFork.c
@@ -11,31 +11,29 @@ void main(int argc, char **argv){
     for(int i = 1; i < argc; i++){
         args[i - 1] = atoi(argv[i]);
     }
-    
-    for(int i = 0; i < args[0]; i++){
-            printf("Macierzysty krok %d\n",i);
-            sleep(1);
-    }
-    
-    
+        
     for(int i = 0; i < argc - 2; i++){//jak wiele procesow chce utworzyc
         if((pid = fork()) == 0){
 
             for(int y = 0; y < args[i + 1]; y++){
                 printf("Krok %d procesu %d \n", y, getpid());
+                sleep(1);
             } 
             exit(getpid());
         }
-        
+    }
+    
+    for(int i = 0; i < args[0]; i++){
+            printf("Macierzysty krok %d\n",i);
+            sleep(1);
+    }
 
+    for(int i = 0; i < argc - 2; i++){    
         pid = wait(&status);
         
         printf("Proces %d zostal zakonczony, status  %d \n", pid, WEXITSTATUS(status));
-            
-            
     }
 
-
-
+    exit(0);
 
 }
