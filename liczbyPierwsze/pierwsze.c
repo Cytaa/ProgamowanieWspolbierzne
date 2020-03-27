64547d76a20ba21409d3f403f4846d722505adcd
cyta <piotrstachnio@gmail.com>
2020-03-27 10:37:14 +0100

    init 2nd task

---
+++ liczbyPierwsze/pierwsze.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    int start = atoi(argv[1]);
+    int inter = atoi(argv[2]) - atoi(argv[1]);
+    
+    printf("przedzial %d", inter);
+
+
+
+    return 0;
+}
